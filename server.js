6bd1b4048a79c01778840750bd1ef7ade17a2b82
msms804 <104570214+msms804@users.noreply.github.com>
2023-06-21 22:03:13 +0900

    Node+Express 서버와 React 연동, install cors

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+//이 코드 넣고 시작하셔야 리액트와 nodejs 서버간 ajax 요청 잘됩니다. 
+app.use(express.json());
+var cors = require('cors');
+app.use(cors());
+
 app.listen(8080, function () {
     console.log('listening on 8080')
-}); 
+});
+
+app.use(express.static(path.join(__dirname, 'mystock-fe/build')));
+
+app.get('/', function (요청, 응답) {
+    응답.sendFile(path.join(__dirname, '/mystock-fe/build/index.html'));
+});
+
+
+//항상 가장하단에
+app.get('*', function (요청, 응답) {
+    응답.sendFile(path.join(__dirname, '/mystock-fe/build/index.html'));
+});
